f2a62c1e049e3adaf224973ed2c36deac1333848
alfonzo65 <60753491+alfonzo65@users.noreply.github.com>
2022-11-06 00:15:22 -0400

    Add files via upload

---
+++ build/CMakeFiles/tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial.dir/visorDeCubos.cpp.obj"
+  "CMakeFiles/tutorial.dir/visorDeCubos.cpp.obj.d"
+  "libtutorial.dll.a"
+  "tutorial.exe"
+  "tutorial.exe.manifest"
+  "tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
